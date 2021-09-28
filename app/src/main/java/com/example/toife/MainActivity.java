b4e6ce3a7d16c1353fbd22160e39a69e703c1452
yashsaxena019 <79358785+yashsaxena019@users.noreply.github.com>
2021-09-28 14:47:08 +0530

    Sign up activity created and made some changes in string and themes

--- app/src/main/java/com/example/toife/MainActivity.java
+++ app/src/main/java/com/example/toife/MainActivity.java
@@ -2,13 +2,33 @@ package com.example.toife;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView su;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        su =(TextView) findViewById(R.id.opensignup);
+        su.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSignup();
+            }
+        });
+    }
+
+    private void openSignup() {
+
+        Intent i = new Intent(this, signup.class);
+        startActivity(i);
+
     }
 }

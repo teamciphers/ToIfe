f3c6f1cdcd30bfd7e52c9c15c5eb69809bb0fe55
yashsaxena019 <79358785+yashsaxena019@users.noreply.github.com>
2021-09-29 23:29:48 +0530

    selfcare homepage finalised

--- app/src/main/java/com/example/toife/homepage.java
+++ app/src/main/java/com/example/toife/homepage.java
@@ -2,13 +2,33 @@ package com.example.toife;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class homepage extends AppCompatActivity {
+
+    TextView sc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        sc = (TextView) findViewById(R.id.selfcare);
+        sc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSCHome();
+            }
+        });
+    }
+
+    private void openSCHome() {
+
+        Intent secahome =  new Intent(this, Selfcare_homepage.class);
+        startActivity(secahome);
+
     }
 }

184ae9166dac0628130199e5240397ee22bc2aa6
VEADER1005 <71756466+VEADER1005@users.noreply.github.com>
2021-09-29 23:49:19 +0530

    Sign in page to homepage temp solution

--- app/src/main/java/com/example/toife/MainActivity.java
+++ app/src/main/java/com/example/toife/MainActivity.java
@@ -5,24 +5,45 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView su;
+    private Button log_in;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //connecting variables to their XML counterparts
         su =(TextView) findViewById(R.id.opensignup);
+        log_in = findViewById(R.id.signin);
+
+
         su.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openSignup();
             }
         });
+
+        log_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openHomepage();
+            }
+        });
+    }
+
+
+    private void openHomepage() {
+
+        Intent OSign = new Intent(this, homepage.class);
+        startActivity(OSign);
+
     }
 
     private void openSignup() {

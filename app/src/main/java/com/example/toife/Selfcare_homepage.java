14462dac48c5607d7c261d4b02172bd06c19d940
yashsaxena019 <79358785+yashsaxena019@users.noreply.github.com>
2021-10-09 18:12:57 +0530

    selfcare music designend

--- app/src/main/java/com/example/toife/Selfcare_homepage.java
+++ app/src/main/java/com/example/toife/Selfcare_homepage.java
@@ -2,13 +2,33 @@ package com.example.toife;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Selfcare_homepage extends AppCompatActivity {
+
+    private Button b;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_selfcare_homepage);
+
+        b = (Button) findViewById(R.id.selfcare_start);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openmusic();
+            }
+        });
+    }
+
+    private void openmusic() {
+
+        Intent i1=new Intent(this, Selfcare_Music.class);
+        startActivity(i1);
+
     }
 }

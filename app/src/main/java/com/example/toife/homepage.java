9e3248d5470fb845449b1f519359cf55f357b4e1
Harshit228 <info.harshit28@gmail.com>
2021-10-10 13:26:37 +0530

    PopUp menu at home page

--- app/src/main/java/com/example/toife/homepage.java
+++ app/src/main/java/com/example/toife/homepage.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.PopupMenu;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class homepage extends AppCompatActivity {
+public class homepage extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
 
     TextView sc;
 
@@ -31,4 +34,28 @@ public class homepage extends AppCompatActivity {
         startActivity(secahome);
 
     }
+    public void showPopup(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+        popup.setOnMenuItemClickListener(this);
+        popup.inflate(R.menu.popupenu_homepage);
+        popup.show();
+    }
+    @Override
+    public boolean onMenuItemClick(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.item1:
+                Toast.makeText(this, "Item 1 clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item2:
+                Toast.makeText(this, "Item 2 clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.item3:
+                Toast.makeText(this, "Item 3 clicked", Toast.LENGTH_SHORT).show();
+                return true;
+            default:
+                return false;
+        }
+    }
+
+
 }

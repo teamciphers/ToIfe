736ae7e30852cfabd313861cdc560e5786195b95
VEADER1005 <71756466+VEADER1005@users.noreply.github.com>
2021-10-10 11:16:11 +0530

    User profile has been made

---
+++ app/src/main/java/com/example/toife/User_profile.java
@@ -0,0 +1,14 @@
+package com.example.toife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class User_profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_profile);
+    }
+}

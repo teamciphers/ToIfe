383ceaa74eb93447a24680905adae573704933fd
VEADER1005 <71756466+VEADER1005@users.noreply.github.com>
2021-10-10 12:06:17 +0530

    Choose activity for self care has been made

---
+++ app/src/main/java/com/example/toife/Self_care_home.java
@@ -0,0 +1,14 @@
+package com.example.toife;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Self_care_home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_self_care_home);
+    }
+}
